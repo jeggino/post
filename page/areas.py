a5cb49247854f576e7ed105750b87aee4df3e9f9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-02-06 11:22:40 +0100

    Update areas.py

--- page/areas.py
+++ page/areas.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 
 # Read the HTML file
-with open('files/BUCK_TK_Fietsroutes.html', 'r') as file:
+with open('files/BUCK_TK_Fietsroutes (1).html', 'r') as file:
     html_content = file.read()
 
 # Render the HTML
