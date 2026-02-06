774689cd5705f4ddc67e1c1437b23302bccc1fd1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-02-06 09:52:43 +0100

    Update and rename yes_2.py to page_2.py

---
+++ page/page_2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+# Read the HTML file
+with open('files/BUCK_TK_Fietsroutes.html', 'r') as file:
+    html_content = file.read()
+
+# Render the HTML
+components.html(html_content, height=600)
