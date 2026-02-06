a602a8cec438c1dacf8102f953ec1d18d9951eca
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-02-06 09:56:14 +0100

    Fix duplicate import of streamlit.components.v1
    Removed duplicate import statement for components.

--- page /web.py
+++ page /web.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-import streamlit.components.v1 as components
-
 components.iframe("https://www.elskenecologie.nl/contact-elsken-ecologie-nh-terschelling/", height=1600)
 
 
